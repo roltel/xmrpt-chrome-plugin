beeba2d0a817a2af68b50f9dacef9df49d41d8c5
roltel <roltel@roltel.net>
2017-10-19 23:26:04 +0100

    up date

--- poolstats.js
+++ poolstats.js
@@ -2,7 +2,7 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = JSON.parse(this.responseText);
-     document.getElementById("poolstats").innerHTML += myObj.pool_statistics.miners + "<br>" + "Hash Rate: " + myObj.pool_statistics.hashRate + "H/s" + "<br>" + "Block Found: " + myObj.pool_statistics.totalBlocksFound ;
+     document.getElementById("poolstats").innerHTML += myObj.pool_statistics.miners + "<br>" + "Hash Rate: " + myObj.pool_statistics.hashRate + "H/s" + "<br>" + "Block Found: " + myObj.pool_statistics.totalBlocksFound + "<br> Last Block Found: " + myObj.pool_statistics.lastBlockFoundTime*1000 + "<br> Payments: " + myObj.pool_statistics.totalPayments ;
     }
 };
 xmlhttp.open("GET", "https://pool.xmr.pt/api/pool/stats", true);
